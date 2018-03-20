bf585676d1704a6496acb87fc323a63d5b9d118c
matsjoonas <mj@codelight.eu>
2018-03-20 16:15:24 +0200

    Setup theme

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,8 +1,6 @@
 // import external dependencies
 import 'jquery';
 
-// Import everything from autoload
-import "./autoload/**/*"
 
 // import local dependencies
 import Router from './util/Router';
