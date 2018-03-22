f798bff983b9fb04632faf68ec137dd65a4e875e
vagrant <vagrant@codelight-trellis>
2018-03-22 08:46:46 +0000

    Setup for style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,4 @@
-<header class="banner">
-  <div class="container">
+<header class="header">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
