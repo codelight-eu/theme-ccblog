e3f196fecd1495d0de2cfaf6c53a39d3e8efab83
Indrek Kaine <indrek.kaine@codelight.eu>
2018-10-11 12:17:43 +0300

    Add favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/x-icon" href="{{ \App\asset_path('images/favicon.ico') }}" />
   @php(wp_head())
   <script>
     (function(d) {
